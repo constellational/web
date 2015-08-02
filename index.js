7decb49a6b19c0bdb0d01a18546aa387b4027bb6
Arpith Siromoney <arpith@feedreader.co>
2015-08-02 20:37:04 +0530

    use render to string

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 var apiURL = 'https://1dhhcnzmxi.execute-api.us-east-1.amazonaws.com/v1';
 var staticURL = 'https://d1gxzanke6jb5q.cloudfront.net';
 
-var get = function(username, id) {
+var React = require('react');
+var views = require('./views');
+
+var fetchJSON = function(username, id) {
+  var url = apiURL + '/' + username;
+  if (id) url += '/' + id;
+  return fetch(url).then(function(res) {
+    return res.json();
+  });
+};
+
+var generateHTML = function(json) {
+  var reactString = React.renderToString(React.createElement(views.Blog, json));
+  return "<html><body>" + reactString + "</body></html>";
 };
 
 exports.handler = function(event, context) {
   console.log(event);
-  get(event.username, event.id).then(context.succeed).catch(context.fail);
+  fetchJSON(event.username, event.id).then(context.succeed).catch(context.fail);
 };
