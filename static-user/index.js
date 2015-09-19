3fb910590c2aef1690918ad337b8583c01339fc5
Arpith Siromoney <arpith@feedreader.co>
2015-09-19 12:35:22 +0530

    use cloudfront url for css and js

--- static-user/index.js
+++ static-user/index.js
@@ -1,6 +1,8 @@
 var apiURL = 'https://1dhhcnzmxi.execute-api.us-east-1.amazonaws.com/v1';
 var staticURL = 'https://d1gxzanke6jb5q.cloudfront.net';
 var STATIC_BUCKET_NAME = 'constellational-static';
+var JS_URL = 'https://d1gxzanke6jb5q.cloudfront.net/main.js';
+var CSS_URL = 'https://d1gxzanke6jb5q.cloudfront.net/style.css';
 
 require("babel/register");
 var views = require('./views');
@@ -51,10 +53,10 @@ function fetchData(username, id) {
 
 function generateHTML(data) {
   console.log("generating html");
-  var cssSrc = staticURL + '/style.css';
-  var scriptSrc = staticURL + '/main.js';
+  var head = "<meta http-equiv='Content-Type' content='text/html; charset=utf-8'><meta name='viewport' content='width=device-width, initial-scale=1' /><link rel='stylesheet' type='text/css' href='" + CSS_URL + "'>";
   var reactString = React.renderToString(React.createElement(views.Articles, data));
-  return "<html><meta http-equiv='Content-Type' content='text/html; charset=utf-8'><meta name='viewport' content='width=device-width, initial-scale=1' /><link rel='stylesheet' type='text/css' href='" + cssSrc + "'><body><div id='react-mount'>" + reactString + "</div></body><script src='" + scriptSrc + "'></script></html>";
+  var body = "<body><div id='react-mount'>" + reactString + "</div></body><script src='" + JS_URL + "'></script></html>";
+  return "<html>" + head + body + "</html>";
 }
 
 function storeStaticFile(key, html) {
