546355565fc6781e3c52f30d7f9b1fdc1e95f269
Arpith Siromoney <arpith@feedreader.co>
2015-09-19 13:40:56 +0530

    update react views - articles are now posts

--- static-post/views.js
+++ static-post/views.js
@@ -1,26 +1,26 @@
 var React = require('react');
 var moment = require('moment');
 
-var Article = React.createClass({
+var Post = React.createClass({
   render: function() {
-    var article = this.props.data;
-    return <div className="article">
-      <h2><a href={'/' + this.props.username + '/' + article.id}>{article.heading}</a></h2>
-      <div>{article.data}</div>;
-      <div className="time">{moment(article.updated).fromNow()}</div>
+    var post = this.props.data;
+    return <div className="post">
+      <h2><a href={'/' + this.props.username + '/' + post.id}>{post.title}</a></h2>
+      <div>{post.data}</div>;
+      <div className="time">{moment(post.updated).fromNow()}</div>
     </div>
   }
 });
 
-var Articles = React.createClass({
+var User = React.createClass({
   render: function() {
     return <div>
       <h1><a href={'/' + this.props.username}>{this.props.username}</a></h1>
-      <div className="articles">
-        {this.props.articles.map(d => <Article username={this.props.username} data={d} key={d.id} />)}
+      <div className="posts">
+        {this.props.posts.map(d => <Post username={this.props.username} data={d} key={d.id} />)}
       </div>
     </div>;
   }
 });
 
-exports.Articles = Articles;
+exports.User = User;
