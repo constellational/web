65c0cc4e0718f0955ed13f564df90183bcd848b4
Arpith Siromoney <arpith@feedreader.co>
2015-09-20 17:34:44 +0530

    use webpack to upload to s3 - fixes #2

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var webpack = require('webpack');
+var S3Plugin = require('webpack-s3-plugin');
 
 module.exports = {
   entry: [
@@ -12,5 +13,19 @@ module.exports = {
   module: {
     loaders: [{loader: 'babel'}],
     postloaders: [{loader: 'uglify'}]
-  }
+  },
+  plugins: [
+    new S3Plugin({
+      include: /.*\.(css|js)/,
+      s3Options: {
+        accessKeyId: process.env.S3_CONSTELLATIONAL_WEB_ACCESS_KEY_ID,
+        secretAccessKey: process.env.S3_CONSTELLATIONAL_WEB_SECRET_ACCESS_KEY,
+        region: 'us-east-1'
+      },
+      s3UploadOptions: {
+        Bucket: 'constellational-web',
+      }
+    })
+  ]
 };
+
