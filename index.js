c5ddf30f838daa84902248f95faf62f96aae7255
Arpith Siromoney <arpith@feedreader.co>
2015-11-30 18:15:33 +0530

    use ejs - fixes #11

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ var USER_URL = 'https://s3.amazonaws.com/constellational-store';
 var POST_URL = 'https://d2nxl7qthm5fu1.cloudfront.net';
 
 require("babel-core/register");
-var serve = require('koa-static');
 var koa = require('koa');
+var serve = require('koa-static');
+var render = require('koa-ejs');
 var fetch = require('node-fetch');
 var React = require('react');
 var ReactDOMServer = require('react-dom/server');
@@ -33,6 +34,8 @@ function fetchPost(username, url) {
   return fetch(POST_URL + '/' + username + '/' + url).then(res => res.json());
 }
 
+render(app, {root: 'templates'});
+
 app.use(serve('public'));
 
 app.use(function *() {
@@ -51,7 +54,7 @@ app.use(function *() {
       return user;
     });
   });
-  this.body = generateHTML(user);
+  yield this.render('layout', {react: user});
 });
   
 app.listen(port);
