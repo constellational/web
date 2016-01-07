98703dc2bfcdcb2953d3ef31ad4235b8c60fb7ed
Arpith Siromoney <arpith@feedreader.co>
2016-01-07 22:45:59 +0530

    send 404 if user not found, and cleanup server js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict'
+
 var USER_URL = 'https://s3.amazonaws.com/constellational-users';
 var POST_URL = 'https://d2gs3048w5buml.cloudfront.net';
 
@@ -15,7 +17,10 @@ var app = koa();
 var port = process.env.PORT || 3000;
 
 function fetchUser(username) {
-  return fetch(USER_URL + '/' + username).then(res => res.json());
+  return fetch(USER_URL + '/' + username).then((res) => {
+    if (res.status != 200) this.throw();
+    else return res.json();
+  });
 }
 
 function fetchPost(username, url) {
@@ -27,23 +32,22 @@ render(app, {root: 'templates'});
 app.use(serve('public'));
 
 app.use(function *() {
-  var splitURL = this.url.split('/');
+  let splitURL = this.url.split('/');
   splitURL.shift();
-  var username = splitURL.shift().toLowerCase();
-  var id = splitURL.shift();
-  var user = yield fetchUser(username).then((user) => {
-    if ((id) && (user.posts.indexOf(id) > 0)) {
-      user.posts.splice(user.posts.indexOf(id), 1);
-      user.posts.unshift(id);
-    }
-    var promiseArr = user.posts.map(id => fetchPost(username, id));
-    return Promise.all(promiseArr).then((data) => {
-      user.posts = data;
-      return user;
-    });
+  let username = splitURL.shift().toLowerCase();
+  let id = splitURL.shift();
+  let user = {};
+  yield fetchUser(username).catch(() => {
+    this.throw('Couldn\'t find that user!', 404);
+  }).then((u) => {
+    user = u;
+    return user.posts.map(id => fetchPost(username, id));
+  }).then(Promise.all).then((data) => {
+    user.posts = data;
+  }).then(() => {
+    let reactString = ReactDOMServer.renderToString(React.createElement(views.User, user));
+    this.render('layout', {react: reactString});
   });
-  var reactString = ReactDOMServer.renderToString(React.createElement(views.User, user));
-  yield this.render('layout', {react: reactString});
 });
   
 app.listen(port);
